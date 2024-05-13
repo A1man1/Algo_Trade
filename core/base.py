005005857a054544ef672d592aebcdb44396ad22
Nandini Shinde <nanshinde.197324@gmail.com>
2024-05-13 09:46:54 +0530

    make changes in base regarding order

--- core/base.py
+++ core/base.py
@@ -5,6 +5,7 @@ import pandas as pd
 import requests
 from abc import ABC, abstractmethod
 from core import log
+import Portfolio
 
 class Trade(BaseModel):
     stock_name: str
@@ -40,6 +41,60 @@ class Position(BaseModel):
 
     def __str__(self) -> str:
         return f"{self.type_} {self.shares}x{self.entry_price}"
+
+
+class IOrder:
+    def __init__(self, portfolio):
+        self.portfolio = portfolio
+        self.order_history = []
+
+    def place_order(self, symbol, quantity, order_type):
+        pass
+
+    def modify_order(self, order_id, new_quantity):
+        pass
+
+    def cancel_order(self, order_id):
+        pass
+
+    def get_order_history(self):
+        return self.order_history
+    
+
+class Order:
+    def __init__(self, portfolio : Portfolio ):
+        self.portfolio = portfolio
+        self.order_history = []
+
+    def place_order(self, symbol, quantity, order_type):
+        if order_type == "buy":
+            self.portfolio.add_stock(symbol, quantity)
+
+            self.order_history.append({"symbol": symbol, "quantity": quantity, "order_type": order_type})
+        elif order_type == "sell":
+            self.portfolio.remove_stock(symbol, quantity)
+            self.order_history.append({"symbol": symbol, "quantity": quantity, "order_type": order_type})
+        else:
+            print("Invalid order type.")
+
+    def modify_order(self, symbol, quantity, order_type):
+        for order in self.order_history:
+            if order["symbol"] == symbol and order["order_type"] == order_type:
+                order["quantity"] = quantity
+                break
+        else:
+            print("Order not found.")
+
+    def cancel_order(self, symbol, quantity, order_type):
+        for order in self.order_history:
+            if order["symbol"] == symbol and order["quantity"] == quantity and order["order_type"] == order_type:
+                self.order_history.remove(order)
+                break
+        else:
+            print("Order not found.")
+
+    def get_order_history(self):
+        return self.order_history
 
 
 class Portfolio:
